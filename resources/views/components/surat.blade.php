e6573b24666c837c9a24b0fc3b294e471be9ebc7
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-05-28 12:34:00 +0700

    Tambah Template Persuratan

--- resources/views/components/surat.blade.php
+++ resources/views/components/surat.blade.php
@@ -1,12 +1,157 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Surat Pengantar SKCK</title>
+    <style>
+        body {
+            font-family: 'Times New Roman', Times, serif;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 700px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid black;
+        }
+        .header, .footer {
+            text-align: center;
+        }
+        .header b {
+            font-size: large;
+        }
+        hr {
+            border: 1px solid black;
+        }
+        .content {
+            margin-top: 20px;
+        }
+        .content table {
+            width: 100%;
+        }
+        .content td {
+            font-size: small;
+            vertical-align: top;
+        }
+        .content .details {
+            margin-top: 20px;
+        }
+        .content .details table {
+            width: 100%;
+        }
+        .content .details td {
+            padding: 2px 5px;
+        }
+        .signatures {
+            margin-top: 50px;
+            display: flex;
+            justify-content: space-between;
+        }
+        .signature {
+            text-align: center;
+        }
+        .signature span {
+            display: block;
+            margin-bottom: 60px;
+        }
+        .signature .name {
+            text-decoration: underline;
+        }
+    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>ini surat</h1>
+    <div class="container">
+    <div class="header" style="text-align: center; font-family: 'Arial', sans-serif; padding: 20px; background-color: #f8f9fa; border-radius: 8px;">
+        <h1 style="margin-bottom: 5px; font-size: 24px; color: #333;">RUKUN WARGA (RW) 11</h1>
+        <h2 style="margin-top: 0; margin-bottom: 5px; font-size: 20px; color: #555;">DESA TANJUNGREJA</h2>
+        <h3 style="margin-top: 0; font-size: 18px; color: #777;">KECAMATAN SUKUN</h3>
+        <hr style="border: 1px solid #ccc; margin-top: 20px;">
+    </div>
+    <div class="content">
+
+            <div class="grid grid-cols-6 text-xs">
+              <p class="col-span-1">No</p>
+              <p class="col-span-1">: ......../RW/........</p>
+            </div>
+            
+            <div class="grid grid-cols-6 text-xs">
+              <p class="col-span-1">Lampiran</p>
+              <p class="col-span-1">: 1 Berkas</p>
+            </div>
+            
+            <div class="grid grid-cols-6 text-xs">
+              <p class="col-span-1">Perihal</p>
+              <p class="col-span-2">: Pengajuan SKCK</p>
+            </div>
+            
+            <div align="right" style="margin-top: 20px;">
+                <span>Malang, 28 Mei 2024</span>
+            </div>
+            <br>
+            <table>
+                <tr>
+                    <td>Kepada Yth,</td>
+                </tr>
+                <tr>
+                    <td>Ketua Kepolisian Sektor Sukun</td>
+                </tr>
+                <tr>
+                    <td>di</td>
+                </tr>
+                <tr>
+                    <td>Tempat</td>
+                </tr>
+            </table>
+            <div class="details" style="margin-top: 20px;">
+                <p>Dengan hormat,<br>
+                Sehubungan dengan permohonan Surat Keterangan Catatan Kepolisian (SKCK) dari:</p>
+                <br></br>
+                <table>
+                    <tr>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>..............................................................................................................</td>
+                    </tr>
+                    <tr>
+                        <td>Tempat/Tgl Lahir</td>
+                        <td>:</td>
+                        <td>..............................................................................................................</td>
+                    </tr>
+                    <tr>
+                        <td>Jenis Kelamin</td>
+                        <td>:</td>
+                        <td>..............................................................................................................</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>..............................................................................................................</td>
+                    </tr>
+                    <tr>
+                        <td>Pekerjaan</td>
+                        <td>:</td>
+                        <td>..............................................................................................................</td>
+                    </tr>
+                </table>
+                <br></br>
+                <p class="text-justify">Yang bersangkutan adalah warga RT [RT] RW [RW] Desa Tanjungreja Kecamatan Sukun, dengan ini kami mengajukan permohonan penerbitan SKCK atas nama tersebut di atas untuk keperluan [keperluan pembuatan SKCK].</p>
+                <p class="text-justify">Demikian surat pengantar ini kami sampaikan, atas perhatian dan kerjasamanya kami ucapkan terima kasih.</p>
+            </div>
+            <br></br>
+            <span>Mengetahui,</span>
+            <div class="signatures">
+                <div class="signature">
+                    <span>Ketua RT [RT]</span>
+                    <span class="name">....................</span>
+                </div>
+                <div class="signature">
+                    <span>Ketua RW 11,</span>
+                    <span class="name">....................</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
