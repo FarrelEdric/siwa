16e2f1d388299ca80a79fabf3854b8b8f4a933f4
FarrelEdric <franciscusfarrel14@gmail.com>
2024-06-05 21:14:26 +0700

    main update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\penduduk_masuk;
 use App\Http\Controllers\keuangan;
 use App\Http\Controllers\register;
 use App\Http\Controllers\loginController;
-use App\Http\Controllers\sosialisasiController;
 use App\Http\Controllers\penduduk;
 use App\Http\Controllers\kegiatan;
 use App\Http\Controllers\dashboardController;
@@ -30,13 +29,6 @@ use Illuminate\Support\Facades\Route;
 // Route::post('/kegiatan/list', [kegiatan::class, 'list']);
 // Route::get('/kegiatan/create', [kegiatan::class, 'create']);
 // Route::post('/kegiatan', [kegiatan::class, 'store']);
-
-// Group route for kegiatan
-
-Route::get('/login', [loginController::class, 'login'])->middleware('guest');
-Route::get('/register', [register::class, 'register'])->name('register');
-Route::post('/register', [register::class, 'register'])->name('register');
-
 
 // Group route for kegiatan
 
@@ -89,9 +81,7 @@ Route::group(['prefix' => 'bantuanSosial'], function () {
 });
 
 // Group route for surat
-Route::group(['prefix' => 'surat'], function () {
 Route::group(['prefix' => 'surat', 'middleware' => 'noRT'], function () {
-
     Route::resource('/', surat::class);
     Route::post('/list', [surat::class, 'list']);
     Route::get('/create', [surat::class, 'create']);
@@ -101,10 +91,7 @@ Route::group(['prefix' => 'surat', 'middleware' => 'noRT'], function () {
 });
 
 // route login
-    Route::get('/', function () {
-    return view('index');
-});
-
+Route::get('/', [kegiatan::class, 'landing']);
 
 
 // Route::post('/register', [register::class, 'register'])->name('register');
@@ -121,23 +108,20 @@ Route::get('penduduk', [penduduk::class, 'index'])->name('penduduk');
 Route::post('penduduk/list', [penduduk::class, 'list']);
 
 // route bansos
-Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk');
-Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
-Route::get('/strukturOrganisasi', [organisasi::class, 'index']);
 Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk')->middleware('noRT');
 Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
+
+
 
 
 //admin
 Route::group(['middleware' => 'auth'], function () {
     //surat admin
-    Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat');
     Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat')->middleware('noRT');
     Route::get('/surat/delete/{id}', [surat::class, 'batalkanSurat'])->name('surat');
 
 
     // route keuangan
-
     Route::group(['prefix' => 'keuangan'], function () {
         Route::get('/', [keuangan::class, 'index'])->name('keuangan');
         Route::post('/list', [keuangan::class, 'list']);
@@ -154,7 +138,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('penduduk/list', [penduduk::class, 'list']);
 
     // route bansos
-    Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk');
     Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk')->middleware('noRT');
     Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
 
@@ -166,20 +149,4 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('organisasi', [organisasiController::class, 'index'])->name('organisasi');
     Route::post('organisasi/list', [organisasiController::class, 'list']);
     Route::get('organisasi/{id}', [organisasiController::class, 'show']);
-
 });
-
-});
-
-Route::group(['prefix' => 'sosialisasi'], function () {
-    //Route Sosialisasi
-    Route::get('', [sosialisasiController::class, 'index']);
-    Route::post('/list', [sosialisasiController::class, 'list']);
-    Route::get('/create', [sosialisasiController::class, 'create']);
-    Route::post('', [sosialisasiController::class, 'store']);
-    Route::get('/{id}', [sosialisasiController::class, 'show']);
-    Route::get('/{id}/edit', [sosialisasiController::class, 'edit']);
-    Route::put('/{id}', [sosialisasiController::class, 'update']);
-    Route::delete('/{id}', [sosialisasiController::class, 'destroy']);
-});
-
