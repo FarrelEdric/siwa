67c0b6246abce57d1a1461848c1867f0fec03b19
MuhammadIrsyadDany <irsyadnny09@gmail.com>
2024-06-06 02:41:09 +0700

    Edit-Edit done

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -114,38 +114,43 @@
           <li class="nav-item">
             <a href="#" class="nav-link" style="color: white;">
               <i class="nav-icon fas"></i>
-              <p>List Penerima</p>
+              <p>Data Faktor</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{url('/bantuanSosial')}}" class="nav-link" style="color: white;">
               <i class="nav-icon fas"></i>
-              <p>Informasi</p>
+              <p>Kriteria</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="./index2.html" class="nav-link" style="color: white;">
               <i class="nav-icon fas"></i>
-              <p>Pengajuan</p>
+              <p>Alternatif</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="./index2.html" class="nav-link" style="color: white;">
+              <i class="nav-icon fas"></i>
+              <p>Alternatif</p>
             </a>
           </li>
         </ul>
       </li>
 
-      <li class="nav-item {{Auth::user()->isAdmin() ? '':'d-none'}}">
-        <a href="{{ url('/user') }}" class="nav-link {{ ($activeMenu == 'user') ? 'active' : '' }}" style="color: white;">
-     
+      <li class="nav-item {{ Auth::user()->isAdmin() ? '' : 'd-none' }}">
+        <a href="{{ url('/user') }}" class="nav-link {{ $activeMenu == 'user' ? 'active' : '' }}" style="color: white;">
+            <!-- Isi dari tautan bisa ditempatkan di sini -->
         </a>
-      </li>
-      <hr class="bg-white">
-      <!-- Tombol Logout -->
-      <li class="nav-item">
+    </li>
+    <hr class="bg-white">
+    <!-- Tombol Logout -->
+    <li class="nav-item">
         <a href="{{ url('/logout') }}" class="nav-link align-self-end" style="color: white;">
-          <i class="nav-icon fas fa-sign-out-alt"></i>
-          <p>Logout</p>
+            <i class="nav-icon fas fa-sign-out-alt"></i>
+            <p>Logout</p>
         </a>
-      </li>
-     
+    </li>    
     </ul>
   </nav>
 </div>
