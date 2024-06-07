1e69e459d126e106143bc47c9dc7470a1aa6afb5
FarrelEdric <franciscusfarrel14@gmail.com>
2024-06-07 23:35:13 +0700

    fix : route

--- routes/web.php
+++ routes/web.php
@@ -93,64 +93,31 @@ Route::group(['prefix' => 'bantuanSosial'], function () {
 
 // Group route for surat
 Route::group(['prefix' => 'surat'], function () {
-Route::group(['prefix' => 'surat', 'middleware' => 'noRT'], function () {
+    Route::group(['prefix' => 'surat', 'middleware' => 'noRT'], function () {
 
-    Route::resource('/', surat::class);
-    Route::post('/list', [surat::class, 'list']);
-    Route::get('/create', [surat::class, 'create']);
-    Route::post('/', [surat::class, 'store']);
-    Route::get('/download/{id}', [surat::class, 'printPDF']);
-    Route::put('konfirmasi/{id}', [surat::class, 'update']);
-});
+        Route::resource('/', surat::class);
+        Route::post('/list', [surat::class, 'list']);
+        Route::get('/create', [surat::class, 'create']);
+        Route::post('/', [surat::class, 'store']);
+        Route::get('/download/{id}', [surat::class, 'printPDF']);
+        Route::put('konfirmasi/{id}', [surat::class, 'update']);
+    });
 
-// route login
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/', [kegiatan::class, 'landing']);
-
-
-// Route::post('/register', [register::class, 'register'])->name('register');
-Route::post('proses_login', [loginController::class, 'proses_login'])->name('proses_login');
-Route::get('logout', [loginController::class, 'logout'])->name('logout');
-Route::post('proses_register', [register::class, 'proses_register'])->name('proses_register');
-
-// route keuangan
-Route::get('keuangan', [keuangan::class, 'index'])->name('keuangan');
-Route::post('keuangan/list', [keuangan::class, 'list']);
-
-// route penduduk
-Route::get('penduduk', [penduduk::class, 'index'])->name('penduduk');
-Route::post('penduduk/list', [penduduk::class, 'list']);
-
-// route bansos
-Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk');
-Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
-Route::get('/strukturOrganisasi', [organisasi::class, 'index']);
-Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk')->middleware('noRT');
-Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
+    // route login
+    Route::get('/', function () {
+        return view('index');
+    });
+    Route::get('/', [kegiatan::class, 'landing']);
 
 
-//admin
-Route::group(['middleware' => 'auth'], function () {
-    //surat admin
-    Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat');
-    Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat')->middleware('noRT');
-    Route::get('/surat/delete/{id}', [surat::class, 'batalkanSurat'])->name('surat');
-
+    // Route::post('/register', [register::class, 'register'])->name('register');
+    Route::post('proses_login', [loginController::class, 'proses_login'])->name('proses_login');
+    Route::get('logout', [loginController::class, 'logout'])->name('logout');
+    Route::post('proses_register', [register::class, 'proses_register'])->name('proses_register');
 
     // route keuangan
-
-    Route::group(['prefix' => 'keuangan'], function () {
-        Route::get('/', [keuangan::class, 'index'])->name('keuangan');
-        Route::post('/list', [keuangan::class, 'list']);
-        Route::get('/create', [keuangan::class, 'create']);
-        Route::post('/', [keuangan::class, 'store']);
-        Route::get('/{id}', [keuangan::class, 'show']);
-        Route::get('/{id}/edit', [keuangan::class, 'edit']);
-        Route::put('/{id}', [keuangan::class, 'update']);
-        Route::delete('/{id}', [keuangan::class, 'destroy']);
-    });
+    Route::get('keuangan', [keuangan::class, 'index'])->name('keuangan');
+    Route::post('keuangan/list', [keuangan::class, 'list']);
 
     // route penduduk
     Route::get('penduduk', [penduduk::class, 'index'])->name('penduduk');
@@ -158,41 +125,75 @@ Route::group(['middleware' => 'auth'], function () {
 
     // route bansos
     Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk');
+    Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
+    Route::get('/strukturOrganisasi', [organisasi::class, 'index']);
     Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk')->middleware('noRT');
     Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
 
-    //PENDUDUK
-    Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
+
+    //admin
+    Route::group(['middleware' => 'auth'], function () {
+        //surat admin
+        Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat');
+        Route::get('admin/surat', [surat::class, 'adminSurat'])->name('surat')->middleware('noRT');
+        Route::get('/surat/delete/{id}', [surat::class, 'batalkanSurat'])->name('surat');
 
 
-    //struktur organisasi
-    Route::get('organisasi', [organisasiController::class, 'index'])->name('organisasi');
-    Route::post('organisasi/list', [organisasiController::class, 'list']);
-    //route sosial
-    Route::get('organisasi/edit/{id}', [organisasiController::class, 'edit']);
-    Route::put('organisasi/{id}', [organisasiController::class, 'update']);
-    Route::get('organisasi/{id}', [organisasiController::class, 'show']);
+        // route keuangan
 
-    //route sosialisasi
-    Route::group(['prefix' => 'sosialisasi'], function () {
-        Route::get('', [sosialController::class, 'index']);
-        Route::post('/list', [sosialController::class, 'list']);
-        Route::get('/create', [sosialController::class, 'create']);
-        Route::post('', [sosialController::class, 'store']);
-        Route::get('/{id}', [sosialController::class, 'show']);
-        Route::get('/{id}/edit', [sosialController::class, 'edit']);
-        Route::put('/{id}', [sosialController::class, 'update']);
-        Route::delete('/{id}', [sosialController::class, 'destroy']);
-    });
-    //route berita
-    Route::group(['prefix' => 'berita'], function () {
-        Route::get('', [beritaController::class, 'index']);
-        Route::post('/list', [beritaController::class, 'list']);
-        Route::get('/create', [beritaController::class, 'create']);
-        Route::post('', [beritaController::class, 'store']);
-        Route::get('/{id}', [beritaController::class, 'show']);
-        Route::get('/{id}/edit', [beritaController::class, 'edit']);
-        Route::put('/{id}', [beritaController::class, 'update']);
-        Route::delete('/{id}', [beritaController::class, 'destroy']);
+        Route::group(['prefix' => 'keuangan'], function () {
+            Route::get('/', [keuangan::class, 'index'])->name('keuangan');
+            Route::post('/list', [keuangan::class, 'list']);
+            Route::get('/create', [keuangan::class, 'create']);
+            Route::post('/', [keuangan::class, 'store']);
+            Route::get('/{id}', [keuangan::class, 'show']);
+            Route::get('/{id}/edit', [keuangan::class, 'edit']);
+            Route::put('/{id}', [keuangan::class, 'update']);
+            Route::delete('/{id}', [keuangan::class, 'destroy']);
+        });
+
+        // route penduduk
+        Route::get('penduduk', [penduduk::class, 'index'])->name('penduduk');
+        Route::post('penduduk/list', [penduduk::class, 'list']);
+
+        // route bansos
+        Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk');
+        Route::get('bantuanSosial', [bantuan_sosial::class, 'index'])->name('penduduk')->middleware('noRT');
+        Route::post('bantuanSosial/list', [bantuan_sosial::class, 'list']);
+
+        //PENDUDUK
+        Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
+
+
+        //struktur organisasi
+        Route::get('organisasi', [organisasiController::class, 'index'])->name('organisasi');
+        Route::post('organisasi/list', [organisasiController::class, 'list']);
+        //route sosial
+        Route::get('organisasi/edit/{id}', [organisasiController::class, 'edit']);
+        Route::put('organisasi/{id}', [organisasiController::class, 'update']);
+        Route::get('organisasi/{id}', [organisasiController::class, 'show']);
+
+        //route sosialisasi
+        Route::group(['prefix' => 'sosialisasi'], function () {
+            Route::get('', [sosialController::class, 'index']);
+            Route::post('/list', [sosialController::class, 'list']);
+            Route::get('/create', [sosialController::class, 'create']);
+            Route::post('', [sosialController::class, 'store']);
+            Route::get('/{id}', [sosialController::class, 'show']);
+            Route::get('/{id}/edit', [sosialController::class, 'edit']);
+            Route::put('/{id}', [sosialController::class, 'update']);
+            Route::delete('/{id}', [sosialController::class, 'destroy']);
+        });
+        //route berita
+        Route::group(['prefix' => 'berita'], function () {
+            Route::get('', [beritaController::class, 'index']);
+            Route::post('/list', [beritaController::class, 'list']);
+            Route::get('/create', [beritaController::class, 'create']);
+            Route::post('', [beritaController::class, 'store']);
+            Route::get('/{id}', [beritaController::class, 'show']);
+            Route::get('/{id}/edit', [beritaController::class, 'edit']);
+            Route::put('/{id}', [beritaController::class, 'update']);
+            Route::delete('/{id}', [beritaController::class, 'destroy']);
+        });
     });
 });
