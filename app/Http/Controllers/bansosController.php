cf389c81863b655d250db881231044d63f49e1a5
FarrelEdric <franciscusfarrel14@gmail.com>
2024-06-11 16:19:50 +0700

    fix : bansos

--- app/Http/Controllers/bansosController.php
+++ app/Http/Controllers/bansosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Models\BansosModel;
+use App\Models\bansosModel;
 use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\DB;
 
